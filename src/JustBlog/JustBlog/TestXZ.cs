ecc830dd16f8e630f24e74b996d6763ea34c8838
chikudoatc <chikudoatc@hotmail.com>
2016-01-19 14:11:22 +0400

    here test

---
+++ src/JustBlog/JustBlog/TestXZ.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JustBlog
+{
+    public class TestXZ : Controller
+    {
+        //
+        // GET: /TestXZ/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
