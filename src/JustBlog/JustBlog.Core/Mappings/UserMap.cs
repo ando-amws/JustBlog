338419e199fbb5427dfd0c2e6b44a400ddebd842
ando-amws <arson.ando@outlook.com>
2016-01-20 11:44:35 +0400

    UserMap - ando

--- src/JustBlog/JustBlog.Core/Mappings/UserMap.cs
+++ src/JustBlog/JustBlog.Core/Mappings/UserMap.cs
@@ -7,15 +7,16 @@ namespace JustBlog.Core.Mappings
     {
         public UserMap()
         {
-            Id(x => x.Id);
-            Map(x => x.firstName).Length(250).Not.Nullable();
-            Map(x => x.lastName).Length(250).Not.Nullable();
-            Map(x => x.Email).Length(250).Not.Nullable();
-            Map(x => x.Username).Length(250).Not.Nullable();
-            Map(x => x.Password).Length(250).Not.Nullable();
-            Map(x => x.Active).Length(5).Not.Nullable();
-            Map(x => x.Session).Length(5).Not.Nullable();
             Table("User");
+            Id(x => x.Id);
+            Map(x => x.firstName).Length(50).Not.Nullable();
+            Map(x => x.lastName).Length(50).Not.Nullable();
+            Map(x => x.Email).Length(50).Not.Nullable();
+            Map(x => x.Username).Length(50).Not.Nullable().Column("username");
+            Map(x => x.Password).Length(50).Not.Nullable().Column("password");
+            Map(x => x.Active).Length(5).Not.Nullable().Column("active");
+            Map(x => x.Session).Length(5).Not.Nullable().Column("session_code");
+            
         }
 
     }
